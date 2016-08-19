ec0a3eead2287ce5b79abc4637b033aba27a8baf
Karl Chen <kn007@users.noreply.github.com>
2016-08-19 18:57:22 +0800

    Update information

--- converter.sh
+++ converter.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # File: converter.sh
-# Date: April 15th, 2016
-# Time: 15:33:52 +0800
+# Date: August 19th, 2016
+# Time: 18:56:52 +0800
 # Author: kn007 <kn007@126.com>
 # Blog: https://kn007.net
+# Link: https://github.com/kn007/silk-v3-decoder
 # Usage: sh converter.sh silk_v3_file/input_folder output_format/output_folder flag(format)
 # Flag: not define   ----  not define, convert a file
 #       other value  ----  format, convert a folder, batch conversion support
